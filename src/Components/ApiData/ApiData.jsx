fa2a43128f4203a086c7a15216ca33d4a1d0876e
AnkushDX <ankushpanwar.dx@gmail.com>
2024-02-21 18:33:14 +0530

    21 feb,2024

--- src/Components/ApiData/ApiData.jsx
+++ src/Components/ApiData/ApiData.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Style from "../ApiData/ApiData.module.css";
 import axios from "axios";
-// import {useNavigate} from "react-router-dom"
+import * as XLSX from "xlsx";
 
 const ApiData = () => {
-  // const navigate = useNavigate()
   const [originalData, setOriginalData] = useState([]);
   const [myData, setMyData] = useState([]);
   const [searchUserId, setSearchUserId] = useState("");
@@ -14,7 +13,7 @@ const ApiData = () => {
   const [loading, setLoading] = useState(true);
   const [perPage, setPerpage] = useState(10);
   const [currentPage, setcurrentPage] = useState(1);
-  const [searchPage, setSearchPage]=useState("")
+  const [searchPage, setSearchPage] = useState("");
 
   const shouldShowpagination = myData.length > perPage && !noData;
 
@@ -27,6 +26,88 @@ const ApiData = () => {
     });
   }, []);
 
+  //..............Function Export Post Data to Excel.........////
+
+  const exportToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(myData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Post");
+
+    const columns = [{ wch: 5 }, { wch: 5 }, { wch: 70 }, { wch: 200 }];
+    worksheet["!cols"] = columns;
+
+    let searchQuery = "";
+    if (searchUserId) {
+      searchQuery += `UserId-${searchUserId}`;
+    }
+    if (searchTitle) {
+      if (searchQuery !== "") {
+        searchQuery += " ";
+      }
+      searchQuery += `Title-${searchTitle}`;
+    }
+
+    const currentDate = new Date();
+    const dateFormatter = new Intl.DateTimeFormat("en-IN", {
+      timeZone: "Asia/Kolkata",
+      timeZoneName: "short",
+      weekday: "long",
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+    const [{ value: day }, ,{ value: month }, ,{ value: year }, ,{ value: hour }, ,{ value: minute }, ,{ value: timeZone }, ,{ value: hour12 }, ] = dateFormatter.formatToParts(currentDate);
+    const formattedTime = `${hour}:(${minute} ${timeZone} ${hour12})`.replace('_',':');
+    const formattedDate = `${day} ${month} ${year} ${formattedTime}`;
+    const fileName = `Post-${searchQuery} ${formattedDate}.xlsx`;
+
+    XLSX.writeFile(workbook, fileName);
+  };
+
+  //..............Function Export Pagination Data to Excel.........////
+  const PaginateDataToExcel = () => {
+    const worksheet = XLSX.utils.json_to_sheet(currentpostPage);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "PaginationData");
+
+    const columns = [{ wch: 10 }, { wch: 10 }, { wch: 70 }, { wch: 200 }];
+    worksheet["!cols"] = columns;
+
+    let searchQuery = "";
+    if (searchUserId) {
+      searchQuery += `UserId-${searchUserId}`;
+    }
+    if (searchTitle) {
+      if (searchQuery !== "") {
+        searchQuery += " ";
+      }
+      searchQuery += `Title-${searchTitle}`;
+    }
+
+    const currentDate = new Date();
+    const dateFormatter = new Intl.DateTimeFormat("en-IN", {
+      timeZone: "Asia/Kolkata",
+      timeZoneName: "short",
+      weekday: "long",
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    });
+    const [{ value: day }, ,{ value: month }, ,{ value: year }, , { value: hour }, ,{ value: minute }, ,{ value: timeZone }, ,{ value: hour12 },] = dateFormatter.formatToParts(currentDate);
+    const formattedDate = `${day} ${month} ${year} ${hour}:(${minute}:${timeZone} ${hour12})`;
+    const fileName = `PaginateData ${searchQuery} ${formattedDate}.xlsx`;
+
+    XLSX.writeFile(workbook, fileName);
+  };
+
+  //...............Pagination Start.........////
+
   const indexLast = currentPage * perPage;
   const indeFpage = indexLast - perPage;
   const currentpostPage = myData.slice(indeFpage, indexLast);
@@ -38,6 +119,8 @@ const ApiData = () => {
   const PageShow = (num) => {
     setcurrentPage(num);
   };
+  //..............Function Export Search Data to Excel.........////
+
   // ......................NextPage..................................
   const Nextpage = () => {
     const lastpage = Math.ceil(myData.length / perPage);
@@ -88,39 +171,42 @@ const ApiData = () => {
     setSearchTitle(e.target.value.trim());
   };
 
-  const handleUserIdFilter = (e) => {
+  const handleSelectUserId = (e) => {
     setSelectedUserId(e.target.value);
+  };
+
+  const handlPageSearch = (e) => {
+    const pageValue = e.target.value.trim();
+    setSearchPage(pageValue);
+    const page = parseInt(pageValue);
+    if (pageValue === "") {
+      setNoData(false);
+      setcurrentPage(1);
+    } else if (
+      isNaN(page) ||
+      page > Math.ceil(myData.length / perPage) ||
+      page <= 0
+    ) {
+      setNoData(true);
+    } else {
+      setNoData(false);
+      setcurrentPage(page);
+    }
   };
 
   const handleReset = () => {
     setSearchTitle("");
     setSearchUserId("");
     setSelectedUserId("");
-    setSearchPage("")
-    setcurrentPage(1)
+    setSearchPage("");
+    setcurrentPage(1);
     setMyData(originalData);
     setNoData(false);
-   
-    // navigate('/')
   };
 
   useEffect(() => {
     handleSearch();
   }, [searchUserId, searchTitle, selectedUserId]);
-
-  useEffect(()=>{
-    if(searchPage !=="" && searchPage >0 && searchPage <= Math.ceil (myData.length/perPage)){
-      setcurrentPage(searchPage)
-    }
-    // else{
-    //   setNoData(true)
-      
-    // }
-  },[searchPage,myData.length,perPage]);
-
-  const handlPageSearch =(e)=>{
-    setSearchPage(e.target.value.trim())
-  }
 
   return (
     <>
@@ -129,6 +215,14 @@ const ApiData = () => {
           <h1>All Api Data Page</h1>
         </div>
         <div className={Style.searchBar}>
+          <input
+            className={Style.sTitle}
+            type="text"
+            id="searchpage"
+            placeholder="Search by page"
+            value={searchPage}
+            onChange={handlPageSearch}
+          />
           <input
             className={Style.sTitle}
             type="text"
@@ -146,7 +240,7 @@ const ApiData = () => {
           />
           <select
             value={selectedUserId}
-            onChange={handleUserIdFilter}
+            onChange={handleSelectUserId}
             className={Style.selecteid}
           >
             <option value=""> Search select by userId </option>
@@ -163,24 +257,21 @@ const ApiData = () => {
           >
             Reset
           </button>
-          <br/>
-          <label className={Style.pageSearch}>Page Search</label>
-          <input
-          className={Style.sTitle}
-            type="text"
-            id="searchpage"
-            placeholder="Search by page"
-           value={searchPage}
-           onChange={handlPageSearch}
-          />
-          <label className={Style.allData}>All Data Search</label>
-          <input
-          className={Style.sTitle}
-            type="text"
-            id="alldata"
-            placeholder="Search all data"
-           
-          />
+          <br />
+        </div>
+        <div>
+          <button
+            className={`btn btn-sm btn-primary  ${Style.exportBtn}`}
+            onClick={exportToExcel}
+          >
+            Export Post-Data
+          </button>
+          <button
+            className={`btn btn-sm btn-primary ms-2 ${Style.exportBtn}`}
+            onClick={PaginateDataToExcel}
+          >
+            Export Paginate-Data
+          </button>
         </div>
         {loading ? (
           <h1>Loading data ...</h1>
@@ -189,7 +280,7 @@ const ApiData = () => {
             <table className="table  table-hover">
               <thead>
                 <tr className={Style.head}>
-                  <th>Id</th>
+                  <th>#</th>
                   <th>UserId</th>
                   <th>Title</th>
                   <th>Body</th>
@@ -221,19 +312,41 @@ const ApiData = () => {
         )}
       </div>
       {shouldShowpagination && (
-        <nav aria-label="Page navigation example" style={{alignItems:"center",justifyContent:"center",display:"flex"}}>
+        <nav
+          aria-label="Page navigation example"
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
           <ul className={`pagination ${Style.pagibtn}`}>
             {" "}
             <li className="page-item">
-              <a className="page-link" href="#" onClick={Prevpage}>Prev</a>
+              <a className="page-link" href="#" onClick={Prevpage}>
+                Prev
+              </a>
             </li>
             {PageNumber.map((number) => (
-              <li key={number} className={`page-item ${currentPage === number ? 'active':''}`}>
-                <a className="page-link" href="#" onClick={() => PageShow(number)}>{number}</a>
+              <li
+                key={number}
+                className={`page-item ${
+                  currentPage === number ? "active" : ""
+                }`}
+              >
+                <a
+                  className="page-link"
+                  href="#"
+                  onClick={() => PageShow(number)}
+                >
+                  {number}
+                </a>
               </li>
             ))}
             <li className="page-item">
-              <a className="page-link" href="#" onClick={Nextpage}>Next</a>
+              <a className="page-link" href="#" onClick={Nextpage}>
+                Next
+              </a>
             </li>
           </ul>
         </nav>
